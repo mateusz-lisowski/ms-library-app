e5b429ae45c6c3a1c9962a7c2274be4a74f0ed24
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-11-23 22:19:37 +0100

    Create discovery server microservice project

---
+++ discovery-server/src/test/java/com/github/mateusz_lisowski/discovery_server/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mateusz_lisowski.discovery_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
