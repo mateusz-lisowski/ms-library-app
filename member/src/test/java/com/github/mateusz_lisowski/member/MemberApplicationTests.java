dfc8dac8ccadbe88f92f7b473fc2b99c544c7815
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-11-23 22:04:10 +0100

    Create member microservice project

---
+++ member/src/test/java/com/github/mateusz_lisowski/member/MemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mateusz_lisowski.member;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
