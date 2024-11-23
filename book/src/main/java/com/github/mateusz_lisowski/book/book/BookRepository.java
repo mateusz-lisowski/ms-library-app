968782ac42429716cb3616b5ac31cfa605071dfb
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-11-24 00:34:02 +0100

    Create book jpa repository interface

---
+++ book/src/main/java/com/github/mateusz_lisowski/book/book/BookRepository.java
@@ -0,0 +1,14 @@
+package com.github.mateusz_lisowski.book.book;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, UUID> {
+
+    List<Book> findAllByAuthor(String author);
+
+}
