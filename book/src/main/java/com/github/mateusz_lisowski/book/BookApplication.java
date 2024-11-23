3a4bab6e500a523d4b8f0d0fed48ca4dd0ecc041
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-11-23 22:03:56 +0100

    Create book microservice project

---
+++ book/src/main/java/com/github/mateusz_lisowski/book/BookApplication.java
@@ -0,0 +1,13 @@
+package com.github.mateusz_lisowski.book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookApplication.class, args);
+	}
+
+}
---
+++ book/src/test/java/com/github/mateusz_lisowski/book/BookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mateusz_lisowski.book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
