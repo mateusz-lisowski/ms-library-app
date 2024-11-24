e9ec34a2ed36b3782dbb19a13fd634f5f09eb2ea
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-11-24 21:34:15 +0100

    Enable feign client and jpa auditing

--- rent/src/main/java/com/github/mateusz_lisowski/rent/RentApplication.java
+++ rent/src/main/java/com/github/mateusz_lisowski/rent/RentApplication.java
@@ -2,8 +2,12 @@ package com.github.mateusz_lisowski.rent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableJpaAuditing
 public class RentApplication {
 
 	public static void main(String[] args) {
