b887540d14b5d8772e2e7399332abb2d4dcffbb4
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-12-13 15:37:04 +0100

    Create basic app routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import {BooksListComponentComponent} from './books-list-component/books-list-component.component';
+import {MembersListComponentComponent} from './members-list-component/members-list-component.component';
+import {HomeComponent} from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    title: 'Home Page',
+  },
+  {
+    path: 'books',
+    component: BooksListComponentComponent,
+    title: 'Books Page',
+  },
+  {
+    path: 'members',
+    component: MembersListComponentComponent,
+    title: 'Members Page',
+  }
+];
