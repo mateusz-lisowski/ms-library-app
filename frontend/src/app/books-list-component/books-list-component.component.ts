cf9694501fc09072d22561873359bdb862ae3d8f
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-12-13 16:06:55 +0100

    Create list of book detail components based on date fetched by book service

--- frontend/src/app/books-list-component/books-list-component.component.ts
+++ frontend/src/app/books-list-component/books-list-component.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {BookService} from '../book.service';
+import {Book} from '../book';
+import {BookDetailsComponent} from '../book-details/book-details.component';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-books-list-component',
-  imports: [],
+  imports: [
+    BookDetailsComponent,
+    NgForOf
+  ],
   templateUrl: './books-list-component.component.html',
   styleUrl: './books-list-component.component.css'
 })
 export class BooksListComponentComponent {
+
+  bookService: BookService = inject(BookService);
+  booksList: Book[] = [];
+
+  constructor() {
+    this.bookService.getBooks().then(books => {
+      this.booksList = books;
+    })
+  }
 
 }
