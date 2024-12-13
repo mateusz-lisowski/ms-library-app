8896cb2c64b3d3281d09f9543348305469638de4
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-12-13 16:07:08 +0100

    Implement component props

--- frontend/src/app/book-details/book-details.component.ts
+++ frontend/src/app/book-details/book-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {Book} from '../book';
 
 @Component({
   selector: 'app-book-details',
@@ -7,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './book-details.component.css'
 })
 export class BookDetailsComponent {
-
+  @Input() book!: Book;
 }
