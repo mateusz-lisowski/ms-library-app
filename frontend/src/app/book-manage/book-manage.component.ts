be8a9739d163e64fb8f943f2cb0a85daa1f85d30
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-12-13 16:25:47 +0100

    Create book manage component

---
+++ frontend/src/app/book-manage/book-manage.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-book-manage',
+  imports: [],
+  templateUrl: './book-manage.component.html',
+  styleUrl: './book-manage.component.css'
+})
+export class BookManageComponent {
+
+}
