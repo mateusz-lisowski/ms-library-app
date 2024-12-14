a768fc66f2f5274443deae07ae03a43ab0f3419a
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-12-14 14:21:53 +0100

    Implement logic for book details component

--- frontend/src/app/book-details/book-details.component.ts
+++ frontend/src/app/book-details/book-details.component.ts
@@ -1,9 +1,12 @@
 import {Component, Input} from '@angular/core';
 import {Book} from '../book';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-book-details',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.css'
 })
