2fdd9f1992403d23e0f1db2cd48c5ba1e7a2b726
mateusz-lisowski <mateusz.lisowski.dev@gmail.com>
2024-12-16 19:22:59 +0100

    Implement members list component logic

--- frontend/src/app/members-list-component/members-list-component.component.ts
+++ frontend/src/app/members-list-component/members-list-component.component.ts
@@ -1,11 +1,45 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {NgForOf} from '@angular/common';
+import {Member} from '../member';
+import {MemberService} from '../member.service';
+import {MemberDetailsComponent} from '../member-details/member-details.component';
 
 @Component({
   selector: 'app-members-list-component',
-  imports: [],
+  imports: [
+    FormsModule,
+    NgForOf,
+    ReactiveFormsModule,
+    MemberDetailsComponent
+  ],
   templateUrl: './members-list-component.component.html',
   styleUrl: './members-list-component.component.css'
 })
 export class MembersListComponentComponent {
+
+  memberService: MemberService = inject(MemberService);
+  membersList: Member[] = [];
+
+
+  constructor() {
+    this.memberService.getMembers().then(members => {
+      this.membersList = members;
+    })
+  }
+
+  addForm = new FormGroup({
+    memberId: new FormControl(''),
+    memberFirstname: new FormControl(''),
+    memberLastname: new FormControl(''),
+  });
+
+  async submitForm() {
+    await this.memberService.submitAddMemberForm(
+      this.addForm.value.memberId!,
+      this.addForm.value.memberFirstname!,
+      this.addForm.value.memberLastname!,
+    );
+  }
 
 }
